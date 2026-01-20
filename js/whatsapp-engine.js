d9bbb3afad24d15ae711bf09b1ede3ba606e2333
katotokabesha-sys <katotokabesha@gmail.com>
2026-01-20 21:30:10 +0200

    Create whatsapp-engine.js

---
+++ js/whatsapp-engine.js
@@ -0,0 +1,11 @@
+// Lignes 5-12
+this.config = {
+    laurent: {
+        phone: "+243822937321", // Votre numéro
+        categories: ["electronics", "appliances", "auto"]
+    },
+    betty: {
+        phone: "+243971455335", // Votre numéro
+        categories: ["clothing", "cosmetics", "pharmacy"]
+    }
+};
