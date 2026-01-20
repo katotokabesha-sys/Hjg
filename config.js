2d1eebcd91c3565d160c9c3725d8a4c0da0f22c9
katotokabesha-sys <katotokabesha@gmail.com>
2026-01-20 21:20:09 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-    WHATSAPP_NUMBERS: {
-        BETTY: "+243971455335",
-        LAURENT: "+243822937321"
-    },
-    SITE_URL: "https://lbksmart.com",
-    ADMIN_PASSWORD: "1234"
-};
