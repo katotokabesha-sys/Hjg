645107eef0474dfbff81925b296bae386fff5f94
katotokabesha-sys <katotokabesha@gmail.com>
2026-01-20 20:53:43 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    WHATSAPP_NUMBERS: {
+        BETTY: "+243971455335",
+        LAURENT: "+243822937321"
+    },
+    SITE_URL: "https://lbksmart.com",
+    ADMIN_PASSWORD: "1234"
+};
